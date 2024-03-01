7c2d22836f5eaf9c8a16b4d20e6c9844cd2ca46d
lunaro <pavelike@gmail.com>
2024-03-01 16:57:39 +0300

    Initial

---
+++ StateMachine/state.gd
@@ -0,0 +1,10 @@
+extends Node
+class_name State
+
+signal Transitioned
+var active := false
+
+func enter() -> void: pass
+func exit() -> void: pass
+func update() -> void: pass
+func physics_update() -> void: pass
