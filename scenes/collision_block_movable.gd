eb7a415de71f06da2e977cc1cf29f72abc46ef08
lunaro <pavelike@gmail.com>
2024-03-03 12:08:30 +0300

    Поиграл с коллизией

--- scenes/collision_block_movable.gd
+++ scenes/collision_block_movable.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends StaticBody2D
 
 
 # Called when the node enters the scene tree for the first time.
@@ -8,5 +8,4 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-
 	pass
--- scenes/move_and_collide.gd
+++ scenes/move_and_collide.gd
@@ -1,12 +1,15 @@
-extends Node2D
+extends AnimatableBody2D
 
+var velocity: Vector2 = Vector2.ZERO
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
+func _physics_process(delta):
+	velocity.x = 100
+	move_and_collide(velocity * delta)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-
 	pass
