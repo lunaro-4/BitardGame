1622cf0dabc3b52bd2e7fd616d943099a2a1bcaf
lunaro <pavelike@gmail.com>
2024-03-06 21:32:06 +0300

    Минорные изменения

--- Scripts/level.gd
+++ Scripts/level.gd
@@ -19,7 +19,7 @@ func get_subject_to_object_direction(subject : Node2D, object : Node2D) -> Vecto
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	rotate_subject_to_object($Enemy, $player)
+	rotate_subject_to_object($StaticEnemy, $player)
 	var to_player_direction = get_subject_to_object_direction($FollowingProj, $player)
 	follow_player.emit(to_player_direction)
 	pass
--- Scripts/smart_enemy.gd
+++ Scripts/smart_enemy.gd
@@ -5,7 +5,6 @@ const speed = 200
 
 @export var player : Node2D
 
-#@onready var nav_agent = $NavigationAgent2D as NavigationAgent2D
 @onready var pathfinder = $PathfindingLogic
 @onready var direction = pathfinder.target_path_direction
 
@@ -19,14 +18,7 @@ func _physics_process(_delta : float):
 	direction = pathfinder.target_path_direction
 	velocity = direction * speed
 	move_and_slide()
-	
 
-# debug func
-#func _on_timer_timeout():
-	#print(velocity)
-	#print(direction)
-	##print(nav_agent.get_next_path_position())
-	
 
 
 
